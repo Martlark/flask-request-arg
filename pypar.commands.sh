7ae8fd724168f45c5c57e93cbbe0f8278c09c438
Martlark <mr_andrew_d_rowe@hotmail.com>
2021-08-30 11:34:28 +1000

    add manual commands to upload

---
+++ pypar.commands.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Notes and commands to upload to pypi
+#
+# https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+# https://pypi.org/project/twine/
+#
+# https://python-packaging.readthedocs.io/en/latest/non-code-files.html
+# edit setup.py
+# to update the version
+
+. venv37/bin/activate
+
+VERSION=$(cat VERSION)
+PACKAGE=flask_request_arg
+
+# setup first
+
+pip install setuptools
+pip install wheel
+pip install twine
+pip install flask
+pip install flask-sqlalchemy
+pip install flask-wtf
+pip install pytest-flask
+
+# on each release
+
+. venv3/bin/activate
+python setup.py sdist bdist_wheel
+twine check dist/${PACKAGE}-${VERSION}*
+twine upload dist/${PACKAGE}-${VERSION}* -u martlark
